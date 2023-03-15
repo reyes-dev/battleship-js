0a2400a90fba839b151f20c784d3ba393d1d50cd
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-03-15 05:08:09 -0700

    Build Gameboard factory function

---
+++ src/gameboardFactory.js
@@ -0,0 +1,31 @@
+const Gameboard = () => {
+  const _createBoard = () => {
+    const board = [];
+    for (let i = 0; i < 10; i++) {
+      board[i] = [];
+      for (let j = 0; j < 10; j++) {
+        board[i][j] = 0;
+      }
+    }
+    return board;
+  };
+  const board = _createBoard();
+
+  const placeShip = (ship, coordinate) => {
+    for (let i = 0; i < ship.shipLength; i++) {
+      board[coordinate[0] + i][coordinate[1]] = ship;
+    }
+  };
+
+  const receiveAttack = (coordinate) => {
+    if (board[coordinate[0]][coordinate[1]] != 0) {
+      board[coordinate[0]][coordinate[1]].hit();
+      board[coordinate[0]][coordinate[1]] = 0;
+    } else {
+      return coordinate;
+    }
+  };
+  return { board, placeShip, receiveAttack };
+};
+
+export { Gameboard };
---
+++ test/gameboardFactory.test.js
@@ -0,0 +1,41 @@
+import { Gameboard } from "../src/gameboardFactory";
+import { Ship } from "../src/shipFactory";
+
+test("should place object at correct coordinate", () => {
+  const gameboard = Gameboard();
+  const ship = Ship(1);
+  gameboard.placeShip(ship, [0, 0]);
+  expect(gameboard.board[0][0]).toBe(ship);
+});
+
+test("should place object with length of 2 at correct coordinates", () => {
+  const gameboard = Gameboard();
+  const ship = Ship(2);
+  gameboard.placeShip(ship, [0, 0]);
+  expect(gameboard.board[0][0]).toBe(ship);
+  expect(gameboard.board[1][0]).toBe(ship);
+});
+
+test("should remove ship from correct coordinate", () => {
+  const gameboard = Gameboard();
+  const ship = Ship(1);
+  gameboard.placeShip(ship, [0, 0]);
+  gameboard.receiveAttack([0, 0]);
+  expect(gameboard.board[0][0]).toEqual(0);
+});
+
+test("should return message of failure to sink ship", () => {
+  const gameboard = Gameboard();
+  const ship = Ship(1);
+  gameboard.placeShip(ship, [0, 0]);
+  expect(gameboard.receiveAttack([1, 0])).toEqual([1, 0]);
+});
+
+test("should call hit on correct ship", () => {
+  const gameboard = Gameboard();
+  const ship = Ship(1);
+  gameboard.placeShip(ship, [0, 0]);
+  expect(ship.isSunk()).toBe(false);
+  gameboard.receiveAttack([0, 0]);
+  expect(ship.isSunk()).toBe(true);
+});

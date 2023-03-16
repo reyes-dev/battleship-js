2d5783df507a4c9c37a692dfacbf644f8edf4dbe
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-03-16 03:22:17 -0700

    Add reveal of square upon click

--- src/displayController.js
+++ src/displayController.js
@@ -1,12 +1,20 @@
 const displayController = (() => {
   const playerBoard = document.querySelector(".player");
   const computerBoard = document.querySelector(".computer");
-
+  const _revealSquare = (board, coord1, coord2, square) => {
+    square.disabled = true;
+    if (board[coord1][coord2] === 0) {
+      square.innerHTML = "v";
+    } else if (typeof board[coord1][coord2] === "object") {
+      square.innerHTML = "x";
+    }
+  };
   const renderPlayerBoard = (board) => {
     for (let i = 0; i < board.length; i++) {
       for (let j = 0; j < board[i].length; j++) {
         let square = document.createElement("button");
         square.innerHTML = "~";
+        square.disabled = true;
         playerBoard.appendChild(square);
       }
     }
@@ -16,6 +24,9 @@ const displayController = (() => {
       for (let j = 0; j < board[i].length; j++) {
         let square = document.createElement("button");
         square.innerHTML = "~";
+        square.addEventListener("click", function () {
+          _revealSquare(board, i, j, square);
+        });
         computerBoard.appendChild(square);
       }
     }
